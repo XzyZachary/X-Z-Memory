9d3176175a1fad0fac97a13b16bd042e886fb2f6
zachary <zacharyxu@amecnsh.com>
2023-08-14 17:54:45 +0800

    init

---
+++ lib/routes.dart
@@ -0,0 +1,13 @@
+class Routes {
+  static const home = "/";
+  static const login = "/login";
+  static const channelNew = "/channel/new";
+  static const channelInvite = "/channel/invite";
+  static const eventNew = "/event/new";
+  static const image = "/image";
+  static const imagePinch = "/image/pinch";
+  static const imagePicker = "/image/picker";
+  static const reaction = "/reaction";
+  static const user = "/user";
+  static const settings = "/settings";
+}
